c30c5d499a86b69f7015f787c9f964740af8ac54
Andrei Mouraviev <andrei.mouraviev@gmail.com>
2019-09-02 14:26:39 -0400

    model checkpoint and early stop

--- andreiNet/neural_net.py
+++ andreiNet/neural_net.py
@@ -1,4 +1,6 @@
 import numpy as np
+import copy
+
 from andreiNet.utils import one_hot_encode, norm_data, shuffle_data, batch_iterator
 from andreiNet.metrics import accuracy
 from andreiNet.losses import cross_entropy, cross_entropy_derivative
@@ -22,13 +24,15 @@ implemented_activations = {'sigmoid': sigmoid,
                            'ReLU': ReLU,
                            'linear': linear}
 implemented_act_derivatives = {'sigmoid': sigmoid_derivative,
-                           'ReLU': ReLU_derivative,
-                           'linear': linear_derivative
+                               'ReLU': ReLU_derivative,
+                               'linear': linear_derivative
                                }
 implemented_losses = {'cross_entropy': cross_entropy, }
 implemented_loss_gradients = {'cross_entropy': cross_entropy_derivative, }
 implemented_metrics = {'accuracy': accuracy,
                        'cross_entropy': cross_entropy, }
+metric_criteria = {'accuracy': 'max',
+                   'cross_entropy': 'min', }
 
 class NeuralNetwork:
     def __init__(self,
@@ -41,7 +45,6 @@ class NeuralNetwork:
                  metrics=None,
                  shuffle=True,
                  verbose=False,
-                 batch_size=10,
                  random_state=1):
         self.hidden = hidden
         self.init_weights = init_weights
@@ -52,7 +55,6 @@ class NeuralNetwork:
         self.mode = mode
         self.verbose = verbose
         self.shuffle = shuffle
-        self.batch_size = batch_size
         self.metrics = metrics
 
     def _set_weight_init(self):
@@ -68,6 +70,7 @@ class NeuralNetwork:
             raise Exception('{} not accepted'.format(self.init_bias))
 
     def _init_history(self):
+        self.metrics.append(self.loss)
         self.trn_metric_hist = {}
         self.val_metric_hist = {}
         if self.metrics is not None:
@@ -131,10 +134,18 @@ class NeuralNetwork:
             raise Exception('{} not accepted'.format(self.loss))
 
     def _encode(self, y, n_classes):
+        self.n_classes = n_classes
         if n_classes is None and self.mode == 'classification':
             self.n_classes = len(set(y))
         y_one_hot = one_hot_encode(y, self.n_classes)
         return y_one_hot
+
+    def _set_early_stop(self, early_stop):
+        self.early_stop = early_stop
+        if early_stop is not None:
+            self.stop_metric, self.patience = self.early_stop
+            if self.stop_metric not in self.metrics:
+                self.metrics.append(self.stop_metric)
 
     def _get_metrics(self, X, y):
         metric_vals = {}
@@ -149,50 +160,110 @@ class NeuralNetwork:
             metric_vals[metric] = metric_func(y, y_pred)
         return metric_vals
 
+    def _update_metrics(self, X, y_ohe, val_data):
+        self.metrics_trn = self._get_metrics(X, y_ohe)
+        if val_data is not None:
+            X_val, y_val = val_data
+            y_val_ohe = self._encode(y_val, self.n_classes)
+            self.metrics_val = self._get_metrics(X_val, y_val_ohe)
+
     def _update_history(self, update_val=False):
         for metric in self.metrics:
             self.trn_metric_hist[metric].append(self.metrics_trn[metric])
             if update_val:
                 self.val_metric_hist[metric].append(self.metrics_val[metric])
 
+    def _get_stop_criteria(self, epoch):
+        stop_criteria = False
+        if self.early_stop is None:
+            return stop_criteria
+        f = -1 if metric_criteria[self.stop_metric] == 'max' else 1
+        current_score = f * self.val_metric_hist[self.stop_metric][-1]
+        if epoch == 1:
+            self.counter = 0
+            self.best_score = current_score
+        else:
+            if current_score < self.best_score:
+                self.counter = 0
+                self.best_score = current_score
+            else:
+                self.counter += 1
+
+        if self.counter > self.patience:
+            stop_criteria = True
+
+        if stop_criteria:
+            print("early stop: epoch {} patience {}".format(epoch,
+                                                            self.patience))
+        return stop_criteria
+
+    def _model_checkpoint(self, current_score, epoch):
+        self.best_val_loss = current_score
+        self.best_model = (copy.deepcopy(self.weights),
+                           copy.deepcopy(self.biases),
+                           epoch)
+
+    def _update_best_model(self, epoch, save_best, val_data):
+        if save_best and (val_data is not None):
+            current_score = self.val_metric_hist[self.loss][-1]
+            if epoch == 1:
+                print('model checkpoint {}'.format(epoch))
+                self._model_checkpoint(current_score, epoch)
+            else:
+                if current_score < self.best_val_loss:
+                    print('model checkpoint {}'.format(epoch))
+                    self.best_val_loss = current_score
+                    self._model_checkpoint(current_score, epoch)
+
+    def _set_best_model(self, save_best):
+        if save_best:
+            print('setting best model from epoch {}'.format(self.best_model[2]))
+            self.weights = self.best_model[0]
+            self.biases = self.best_model[1]
+
+    def _get_loss(self, X, y):
+        y_pred = self.predict(X)
+        return self.loss_func(y, y_pred)
+
     def train(self, X, y,
               val_data=None,
               n_epochs=10, lr=0.001,
-              n_classes=None):
+              batch_size=10,
+              n_classes=None,
+              save_best=False,
+              early_stop=None):
 
+        self._set_early_stop(early_stop)
         self.n_samples, self.n_features = X.shape
+        #n_batches = np.ceil(self.n_samples / batch_size)
         y_one_hot = self._encode(y, n_classes)
 
-        if val_data is not None:
-            X_val, y_val = val_data
-            y_val_ohe = self._encode(y_val, n_classes)
-
         self._init_neural_network()
-
         for e in range(1, n_epochs + 1):
             self.loss_e = 0
             # shuffle data
             if self.shuffle:
                 X, y_one_hot = shuffle_data(X, y_one_hot)
             # iterate through batches
-            for X_batch, y_batch in batch_iterator(X, y_one_hot, self.batch_size):
+            for X_batch, y_batch in batch_iterator(X, y_one_hot, batch_size):
                 self._feed_forward(X_batch)
                 self._back_prop(X_batch, y_batch, lr)
-                self.loss_batch = self.loss_func(y_batch, self.activations[-1])
-                self.loss_e += self.loss_batch
-
-            self.metrics_trn = self._get_metrics(X, y_one_hot)
-
-            if val_data is not None:
-                self.metrics_val = self._get_metrics(X_val, y_val_ohe)
+                #self.loss_batch = self.loss_func(y_batch, self.activations[-1])
+                #self.loss_e += self.loss_batch / n_batches
+            self.loss_e = self._get_loss(X, y_one_hot)
+            self._update_metrics(X, y_one_hot, val_data)
             self._update_history(update_val=(val_data is not None))
-
-        if self.verbose or e == n_epochs:
-            print('epoch {}: final trn loss = {} trn metrics {}'.format(e,
-                                                                        self.loss_e,
-                                                                        self.metrics_trn))
-            if val_data is not None:
-                print('val metrics {}'.format(self.metrics_val))
+            self._update_best_model(e, save_best, val_data)
+            stop_criteria = self._get_stop_criteria(e)
+            if stop_criteria:
+                break
+            if self.verbose or e == n_epochs:
+                print('epoch {}: final trn loss = {} trn metrics {}'.format(e,
+                                                                            self.loss_e,
+                                                                            self.metrics_trn))
+                if val_data is not None:
+                    print('val metrics {}'.format(self.metrics_val))
+        self._set_best_model(save_best)
 
     def _feed_forward(self, X):
         self.activations = []
