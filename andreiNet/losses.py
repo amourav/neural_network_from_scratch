fcb91b45d56fbed8338334d972106ba6dc771cb5
Andrei Mouraviev <andrei.mouraviev@gmail.com>
2019-10-31 17:59:29 -0400

    focal loss demo started

--- andreiNet/losses.py
+++ andreiNet/losses.py
@@ -49,6 +49,66 @@ class CrossEntropy(Loss):
         return 'cross_entropy'
 
 
+class FocalLoss(Loss):
+    # https://arxiv.org/pdf/1708.02002.pdf
+    def __init__(self, gamma=2.0, alpha=None):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = float(gamma)
+        assert (gamma >= 0)
+
+    def _set_alpha(self, y_true):
+        """
+        set class weight parameter (alpha)
+        :param y_true: ground truth targets - npy arr
+        :return: alpha - 1.0 (float) or numpy array (n_classes)
+        """
+        if self.alpha is None:
+            self.alpha = 1.0
+        elif self.alpha is 'inverse_freq':
+            elements, freq = np.unique(y_true.argmax(axis=1),
+                                       return_counts=True)
+            self.alpha = np.ones(shape=len(elements)) / freq
+        elif type(self.alpha) is list:
+            self.alpha = np.array(self.alpha)
+        else:
+            raise Exception('alpha ({}) not accepted'.format(self.alpha))
+
+    def loss(self, y_true, y_pred):
+        """
+        focal loss for classification tasks
+        :param y_true: ground truth targets - npy arr
+        :param y_pred: predicted targets - npy arr
+        :return: evaluated cross entropy - float
+        """
+
+        self._set_alpha(y_true)
+        # down weight easy examples
+        weight = self.alpha * (1 - y_true * y_pred) ** self.gamma
+
+        N = len(y_true)
+        return -np.sum(y_true * weight * np.log(y_pred)) / N
+
+    def grad(self, y_true, y_pred):
+        """
+        Derivative of focal loss
+        https://www.wolframalpha.com/input/?i=-%281-x%29%5Ek*log%28x%29
+        :param y_true: ground truth targets - npy arr
+        :param y_pred: predicted targets - npy arr
+        :return: gradient of loss - npy arr
+        """
+        tmp = 1 - y_pred
+        N = len(y_true)
+        tmp2 = self.alpha * y_true * tmp ** self.gamma / N
+        return -tmp2 * (self.gamma * np.log(y_pred) / tmp + 1 / y_pred)
+
+    def __repr__(self):
+        return 'focal_loss'
+
+    def __str__(self):
+        return 'focal_loss'
+
+
 class MSE(Loss):
     def __init__(self):
         super().__init__()
